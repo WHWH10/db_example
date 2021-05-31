f20d101369ed1cd1bab05c34f3367e496856805a
WHWH10 <eyet010@gmail.com>
2021-05-31 14:53:13 +0900

    NAVER_CLOUD_OBJECT

--- routes/file/index.js
+++ routes/file/index.js
@@ -5,6 +5,9 @@ var path = require('path');
 var multer = require('multer')
 var upload = multer({ dest: './uploads' })
 
+var AWS = require('aws-sdk')
+var createBucket = require('../../config/createBucket')
+
 var router = express.Router();
 //https://brunch.co.kr/@daniellim/43 참고할것 
 dotenv.config({
@@ -13,6 +16,10 @@ dotenv.config({
         process.env.NODE_ENV == "production" ? ".env" : ".env.dev"
     )
 })
+
+
+const ID = process.env.NAVER_CLOUD_KEY_ID
+const SECRET = process.env.NAVER_CLOUD_SECRET_KEY
 
 /* GET home page. */
 //router.get('/', function(req, res, next) {
@@ -23,12 +30,28 @@ router.get('/', (req, res) => {
     res.render('file_upload')
 })
 
+//router.post('/', upload.single('userfile'), (req, res) => {
+//    console.log(res)
+//    res.json({
+//        resultCode: 200,
+//        resultMessage: 'Upload Success' + req.file
+//    })
+//})
+
 router.post('/', upload.single('userfile'), (req, res) => {
-    console.log(res)
-    res.json({
-        resultCode: 200,
-        resultMessage: 'Upload Success' + req.file
-    })
+    let myFileName = req.file.originalname.split(".")
+    const fileType = myFileName[myFileName.length - 1]
+
+    console.log('myFileName: ' + myFileName)
+    console.log('myFileType: ' + fileType)
+
+    const params = {
+        Bucket: process.env.NAVER_CLOUD_BUCKET_NAME,
+        Key: myFileName[0] + fileType,
+        Body: req.file.buffer,
+    }
+
+    console.log(process.env.NAVER_CLOUD_BUCKET_NAME)
 })
 
 module.exports = router;
